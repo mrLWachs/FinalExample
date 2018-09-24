45513f0465288a77567a693d0c410819ac5864d1
Mr. Wachs <mrLWachs@gmail.com>
2018-09-24 09:14:45 -0500

    New 2018-2019 School Year

--- src/year2019/cs40s/systemfilemanager/FolderBuilder.java
+++ src/year2019/cs40s/systemfilemanager/FolderBuilder.java
@@ -1,10 +1,12 @@
+
 /** required package class namespace */
 package year2019.cs40s.systemfilemanager;
 
+/** required imports */
 import java.io.File;
 
 /**
- * FolderBuilder.java -
+ * FolderBuilder.java - the logic for copying a folder structure
  *
  * @author Mr. Wachs
  * @since 19-Sep-2018
@@ -15,22 +17,39 @@ public class FolderBuilder
     private File targetFolder;
     private File destinationFolder;
 
+    /**
+     * Sets the target folder to the parameter
+     * 
+     * @param targetFolder the target folder for the application
+     */
     public void buildbuildSubFolderStructure(File targetFolder) {
         this.targetFolder = targetFolder;
     }
 
+    /**
+     * Sets the destination folder to the parameter
+     * 
+     * @param destinationFolder the destination folder for the application
+     */
     public void copyOverFolderStructure(File destinationFolder) {
         this.destinationFolder = destinationFolder;
         copy(destinationFolder, targetFolder);
     }
 
+    /**
+     * Makes a copy of the target folder into the destination folder without
+     * any of its files. It also recursively does this for all sub folders
+     * 
+     * @param destinationFolder the destination folder to copy into
+     * @param targetFolder the target folder to copy from 
+     */
     private static void copy(File destinationFolder, File targetFolder) {  
         File newFolder = new File(destinationFolder.getAbsolutePath() + 
                 File.separator + targetFolder.getName());        
         newFolder.mkdir();        
         File[] files = targetFolder.listFiles();
         if (files != null) {    // not yet done, not at the bottom of the tree             
-            for (File file : files) {   // go through all its sub folders
+            for (File file : files) {       // go through all its sub folders
                 if (file.isDirectory()) {
                   copy(newFolder, file);
                 }
