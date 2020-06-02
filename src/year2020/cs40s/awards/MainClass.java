fa825a773034d0ba35216fc37a92b2100b31b1ac
Mr. Wachs <mrLWachs@gmail.com>
2020-06-01 20:54:05 -0500

    The in-class examples

--- src/year2020/cs40s/awards/MainClass.java
+++ src/year2020/cs40s/awards/MainClass.java
@@ -22,7 +22,7 @@ public class MainClass
      */
     public static void main(String[] args) {
         DataStructures.initialize();
-        MainUIView mainUI = new MainUIView(); 
+        MainUIView mainUI = new MainUIView();  
     }
 
 }
